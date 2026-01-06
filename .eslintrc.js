b370cb75ebe6f03f6a7bd4f044d767f121ef36d9
Yash <yashgupta9437@gmail.com>
2026-01-06 15:27:50 +0530

    refactor: update ESLint configuration to include TypeScript recommendations

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,23 @@ module.exports = {
   root: true,
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   rules: {
-    'react-native/no-inline-styles': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
 };
